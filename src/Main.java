021da160157594e1a8a29b4b8d5207f7a890aa8e
Rob Thomure <robthomure@outlook.com>
2022-06-29 22:57:28 -0500

    Stage 2: simple encryption,  reads an English message and an integer number (key) from the standard input and shifts each letter by the specified number according to its order in the alphabet.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int key = scanner.nextInt();
+        String encryptedText = encrypt(text, key);
+        System.out.println(encryptedText);
+    }
+
+    public static String encrypt(String text, int key) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                if (text.charAt(i) + key > 122) {
+                    stringBuilder.append((char) ((text.charAt(i) + key) % 122 + 96));
+                } else {
+                    stringBuilder.append((char) (text.charAt(i) + key));
+                }
+
+            } else {
+                stringBuilder.append(text.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void stage1Implementation() {
         String message = "we found a treasure!";
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < message.length(); i++) {
